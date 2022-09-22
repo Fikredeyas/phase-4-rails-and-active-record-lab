af562c782b3eaa2261950cef9db7b5de9353e6b3
Fikremariam Deyas <fikredeyas1@gmail.com>
2022-09-22 14:48:33 -0400

    commit tree 45740fe1ba7dcdb03e4d5a6871664f0a98a661b2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "chicklete", last_name: "breg", grade: 2  )
+Student.create(first_name: "rari", last_name: "458", grade: 6  )
+Student.create(first_name: "rockhead", last_name: "dick", grade: 0  )
+
+
